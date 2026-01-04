6ce7e763f016c86f27c62a371afe9bb378c68549
bhavikdodda <bhavikdodda@gmail.com>
2026-01-04 10:06:04 +0530

    left device

---
+++ basic_drum.py
@@ -0,0 +1,174 @@
+import socket
+import time
+import pygame
+from collections import deque
+from pathlib import Path
+
+# -----------------------------
+# CONFIG
+# -----------------------------
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5005
+DURATION_SECONDS = 120
+
+WINDOW_SIZE = 50
+FEATURES_PER_ROW = 6
+# -----------------------------
+
+# -----------------------------
+# AUDIO INIT (LOW LATENCY)
+# -----------------------------
+pygame.mixer.pre_init(44100, -16, 2, 256)
+pygame.init()
+
+BASE_DIR = Path(__file__).resolve().parent
+SNARE_PATH = BASE_DIR / "sound" / "snare.mpeg"
+
+SNARE = pygame.mixer.Sound(str(SNARE_PATH))
+# -----------------------------
+
+# -----------------------------
+# SLIDING WINDOW
+# -----------------------------
+window = deque(maxlen=WINDOW_SIZE)
+# -----------------------------
+
+# -----------------------------
+# MODEL
+# -----------------------------
+def score(input):
+    if input[146] <= -0.5661009848117828:
+        if input[32] <= -0.4818115085363388:
+            var0 = [0.0, 1.0, 0.0]
+        else:
+            var0 = [0.0, 0.4, 0.6]
+    else:
+        if input[187] <= -0.6529539823532104:
+            if input[247] <= -0.6609494984149933:
+                if input[98] <= -0.28472900390625:
+                    if input[296] <= -0.28155550360679626:
+                        var0 = [0.0, 1.0, 0.0]
+                    else:
+                        var0 = [1.0, 0.0, 0.0]
+                else:
+                    if input[110] <= 0.021667500026524067:
+                        var0 = [1.0, 0.0, 0.0]
+                    else:
+                        var0 = [0.4, 0.6, 0.0]
+            else:
+                if input[217] <= -0.6558839976787567:
+                    if input[77] <= 44.58618927001953:
+                        var0 = [1.0, 0.0, 0.0]
+                    else:
+                        var0 = [0.2, 0.0, 0.8]
+                else:
+                    if input[195] <= 3.6621105074882507:
+                        var0 = [0.0, 0.0, 1.0]
+                    else:
+                        var0 = [0.2, 0.2, 0.6]
+        else:
+            if input[157] <= -0.6719360053539276:
+                if input[197] <= 39.794921875:
+                    if input[38] <= -0.2765499949455261:
+                        var0 = [0.8, 0.2, 0.0]
+                    else:
+                        var0 = [1.0, 0.0, 0.0]
+                else:
+                    var0 = [0.0, 0.0, 1.0]
+            else:
+                if input[149] <= 14.404300689697266:
+                    if input[221] <= 21.026615142822266:
+                        if input[278] <= -0.16418449580669403:
+                            var0 = [0.0, 1.0, 0.0]
+                        else:
+                            if input[153] <= -2.777100592851639:
+                                var0 = [0.0, 0.0, 1.0]
+                            else:
+                                var0 = [0.0, 0.6, 0.4]
+                    else:
+                        var0 = [0.0, 0.0, 1.0]
+                else:
+                    var0 = [0.0, 0.0, 1.0]
+    return var0
+# -----------------------------
+
+# -----------------------------
+# AUDIO TRIGGER
+# -----------------------------
+def play_snare():
+    base_dir = Path(__file__).resolve().parent.parent   # project root
+    sound_path = base_dir / "sound" / "snare.wav"
+
+    if not pygame.mixer.get_init():
+        pygame.mixer.pre_init(44100, -16, 2, 256)
+        pygame.init()
+
+    snare = pygame.mixer.Sound(str(sound_path))
+    snare.play()
+    time.sleep(snare.get_length())
+    
+
+# -----------------------------
+# UDP SOCKET
+# -----------------------------
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+# -----------------------------
+def window_variation(window):
+    total_var = 0.0
+
+    # for each of the 6 features
+    for c in range(6):
+        for t in range(1, len(window)):
+            total_var += abs(window[t][c] - window[t-1][c])
+
+    return total_var
+
+def row_variation(row):
+    total_var = 0.0
+    for t in range(1, len(row)):
+        total_var += abs(row[t] - row[t-1])
+
+    return total_var
+# -----------------------------
+# START
+# -----------------------------
+print("Recording will start in 5 seconds...")
+for i in range(3, 0, -1):
+    print(i)
+    time.sleep(1)
+
+start_time = time.time()
+print("Listening...")
+VARTHRESHOLD=60
+try:
+    while (time.time() - start_time) < DURATION_SECONDS:
+        data, _ = sock.recvfrom(1024)
+        decoded = data.decode().strip()
+
+        row = list(map(float, decoded.split(",")))[2:]
+        print("row",row)
+
+        window.append(row)
+        print(row_variation(row))
+        if len(window) == WINDOW_SIZE:
+            # flatten 50 x 6 → 300
+            input_vector = [x for r in window for x in r]
+
+            output = score(input_vector)
+            if row_variation(row) > VARTHRESHOLD:
+                if output[2] == 1.0:
+                    pygame.mixer.Sound("sound/snare.mpeg").play()
+                    print("Played Snare")
+                elif output[0]==1.0:
+                    pygame.mixer.Sound("sound/hihat.mpeg").play()
+                    print("Played HiHat")
+                
+
+except KeyboardInterrupt:
+    print("Stopped by user.")
+
+finally:
+    sock.close()
+    pygame.quit()
+    print("Done.")
